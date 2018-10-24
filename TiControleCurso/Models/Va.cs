cd678989a2a5cce2625c2b5326b37ba736d9b420
jonathan.vieira <jonathan.vieira@cebraspe.org.br>
2018-10-24 17:46:11 -0300

    continuação do script de aulas

---
+++ TiControleCurso/Models/Va.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TiControleCurso.Models
+{
+    public class Va
+    {
+        public string Uia { get; set; }
+        public int IdUsuario { get; set; }
+        public string Status { get; set; }
+    }
+}
--- TiControleCurso/Scripts/app.js
+++ TiControleCurso/Scripts/app.js
@@ -1,19 +1,39 @@
 ﻿
-
+ 
 var iddamateria = 0;
 var iddauia = 0;
 var aulas = [];
-var inputs = document.getElementsByClassName("aulauia1");
+var inputsAulas = document.getElementsByClassName("aulasuia" + iddauia);
+var inputsVas = document.getElementsByClasseName("in");
+var comandoSalvar = 0;
 
 $(".botaoSalvar").click(function () {
     iddauia = $(this).attr('iddauia');
     iddamateria = $(this).attr('iddamateria');
+    comandoSalvar = $(this).attr('iddocomando');
+    if (comandoSalvar = 1) {
+        SalvarAulas();
+    }
+    if (comandoSalvar = 2) {
+        SalvarVas();
+    }
+    if (comandoSalvar = 3) {
+        SalvarForum();
+    }
+    if (comandoSalvar = 4) {
+        SalvarExercicios();
+    }
+})
+
+//Aulas
+
+function SalvarAulas() {
     VerificarAulas();
     $.ajax({
         type: 'GET',
         url: 'Home/InserirAulas',
         dataType: 'json',
-        data: JSON.stringify(aulas),
+        data: { listaAulas: JSON.stringify(aulas) },
         success: function () {
             alert("salvo");
         },
@@ -21,26 +41,32 @@ $(".botaoSalvar").click(function () {
             console.log(e);
         }
     });
-
-})
+};
 
 function VerificarAulas() {
     var i = 0;
     
-    for (i = 0; i < inputs.length; i++) {
-        var naula = 1 + i;
-        if ($("#aula" + naula + "uia" + iddauia + "").prop('checked') == true) {
-            var aula = {
-                NomeAula: "Aula" + naula + "",
-                Status: "Completa",
-                IdUsuario: 1,
-                Uia: "UIA " + iddauia + "",
-                Materia: iddamateria
-            };
+    for (i = 0; i < inputsAulas.length; i++) {
+
+        var naula = parseInt(inputsAulas[i].id.slice(4, 5));
+        if ($("#aula" + naula + "uia" + iddauia).prop('checked') == true) {
+            var aula = {};
+            aula.NomeAula = "Aula" + naula;
+            aula.Status = "Completa";
+            aula.IdUsuario = 1;
+            aula.Uia = "UIA " + iddauia;
+            aula.Materia = iddamateria;
+
             aulas.push(aula);
         }
     }
 };
+
+//Fim Aulas 
+
+
+
+
 
 
 
